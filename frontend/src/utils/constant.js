9aed80fcf2e3de2bf411a155574d65c55c1d331c
Yash Raval <yashraval201@gmail.com>
2024-08-30 18:17:09 +0530

    constant.js update

--- frontend/src/utils/constant.js
+++ frontend/src/utils/constant.js
@@ -1,11 +1,4 @@
-<<<<<<< HEAD
 export const USER_API_END_POINT="https://job-hunt-mern-stack.vercel.app/api/v1/user";
 export const JOB_API_END_POINT="https://job-hunt-mern-stack.vercel.app/api/v1/job";
 export const APPLICATION_API_END_POINT="https://job-hunt-mern-stack.vercel.app/api/v1/application";
 export const COMPANY_API_END_POINT="https://job-hunt-mern-stack.vercel.app/api/v1/company";
-=======
-export const USER_API_END_POINT="https://job-hunt-portal.onrender.com/api/v1/user";
-export const JOB_API_END_POINT="https://job-hunt-portal.onrender.com/api/v1/job";
-export const APPLICATION_API_END_POINT="https://job-hunt-portal.onrender.com/api/v1/application";
-export const COMPANY_API_END_POINT="https://job-hunt-portal.onrender.com/api/v1/company";
->>>>>>> 3f9735edd37f537bfd955670f251fa59c63c83f9
